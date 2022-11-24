09f553338c79a03bac0a7fd23e4b51842c9312bb
aldy031 <117900897+aldy031@users.noreply.github.com>
2022-11-24 18:03:10 +0800

    Update 1_D_71220922.py

--- 1_D_71220922.py
+++ 1_D_71220922.py
@@ -1,4 +1,36 @@
-satu = int(input("masukkan bilangan pertama"))
-dua = int(input("masukkan bilangan kedua"))
-perkalian = satu * dua
-print (" hasil dari {} dijumlahkan dengan {} adalah {}".format(satu,dua,perkalian))
+print ('Program Kalkulator Sederhana')
+print ('1. untuk menghitung Penjumlahan')
+print ('2. untuk menghitung Penjumlahan')
+print ('3. untuk menghitung Penjumlahan')
+print ('4. untuk menghitung Penjumlahan')
+print ('5. untuk menghitung Penjumlahan')
+print ('6. untuk menghitung Penjumlahan')
+print ('silahkan pilih 1-6')
+
+ 
+
+pil = int(input ('Masukkan pilihan : '))
+if pil == 1:
+    a = int(input("bil 1 = "))
+    b = int(input("bil 2 = "))
+    print(f"hasil dari {a} dijumlahkan dengan {b} adalah", a+b)
+elif pil == 2:
+    a = int(input("bil 1 = "))
+    b = int(input("bil 2 = "))
+    print(f"hasil dari {a} dijumlahkan dengan {b} adalah", a-b)
+elif pil == 3:
+    a = int(input("bil 1 = "))
+    b = int(input("bil 2 = "))
+    print(f"hasil dari {a} dijumlahkan dengan {b} adalah ",a*b)
+elif pil == 4:
+    a = int(input("bil 1 = "))
+    b = int(input("bil 2 = "))
+    print(f"hasil dari {a} dijumlahkan dengan {b} adalah", a/b)
+elif pil == 5:
+    a = int(input("bil 1 = "))
+    b = int(input("bil 2 = "))
+    print(f"hasil dari {a} dijumlahkan dengan {b} adalah", a%b)
+elif pil == 6:
+    a = int(input("bil 1 = "))
+    b = int(input("bil 2 = "))
+    print(f"hasil dari {a} dijumlahkan dengan {b} adalah", a**b)
