df75fb6c83453060a013f9310667c86a50a63cea
aldy031 <117900897+aldy031@users.noreply.github.com>
2022-11-24 17:36:36 +0800

    Add files via upload

---
+++ 1_D_71220922.py
@@ -0,0 +1,4 @@
+satu = int(input("masukkan bilangan pertama"))
+dua = int(input("masukkan bilangan kedua"))
+perkalian = satu * dua
+print (" hasil dari {} dijumlahkan dengan {} adalah {}".format(satu,dua,perkalian))
